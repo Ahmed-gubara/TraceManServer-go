9d5241283b42a5bfec643430b92db09c3cf07b89
Ahmed-gubara <ahmed.gubara93@gmail.com>
2020-02-07 10:17:30 +0200

    rtest

--- telegram/telegramService.go
+++ telegram/telegramService.go
@@ -169,6 +169,18 @@ func startTCPServer(bot *bot_api.BotAPI) {
 }
 func handleTCPConnection(conn net.Conn, bot *bot_api.BotAPI) {
 	broadCastMessage(bot, fmt.Sprintf("connection started with ip <pre>%s</pre>", conn.RemoteAddr().String()))
+	broadCastMessage(bot, fmt.Sprintf(`
+<b>bold</b>, <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<b>bold <i>italic bold <s>italic bold strikethrough</s> <u>underline italic bold</u></i> bold</b>
+<a href="http://www.example.com/">inline URL</a>
+<a href="tg://user?id=123456789">inline mention of a user</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>`))
+
 	defer func() {
 		conn.Close()
 		broadCastMessage(bot, fmt.Sprintf("connection closed from ip <pre>%s</pre>", conn.RemoteAddr().String()))
